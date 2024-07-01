41301fea3cb4bfbed536b6c706f109de1cb369ec
anirudhkala110 <anirudhkala110@gmail.com>
2024-07-01 23:24:03 +0530

    Updated Site for Home Stay with components and other folders updated small things navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './Utils/Navbar';
 import Homepage from './Assists/Homepage';
 import SupportiveNav from './Utils/SupportiveNav';
+import AboutPage from './Assists/AboutPage';
+import ContactPage from './Assists/ContactPage';
 
 function App() {
   return (
-    <div className="container-fluid min-vh-100 px-0">
-      <SupportiveNav />
-      <Navbar />
+    <div className="container-fluid min-vh-100 px-0" style={{ minWidth: "350px" }}>
       <div className=''>
         <Router>
+          <SupportiveNav />
+          <Navbar />
           <Routes>
             <Route exact path='/' element={<Homepage />} />
+            <Route exact path='/about' element={<AboutPage />} />
+            <Route exact path='/contact' element={<ContactPage />} />
           </Routes>
         </Router>
       </div>
--- frontend/src/Utils/Navbar.jsx
+++ frontend/src/Utils/Navbar.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
 import './Navbar.css'
+import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
-        <nav class="navbar sticky-top navbar-dark bg-dark">
-            <div class="container-fluid">
+        <nav id="navbar" className="navbar navbar-expand-lg bg-dark d-flex sticky-top justify-content-between align-items-center">
+            <div className='d-flex align-items-center'>
+                <a className="navbar-brand" href="/"><figure className="mb-0 d-flex align-items-center justify-content-center">
+                    {/* <img src={Logo} style={{ maxHeight: '45px', maxWidth: '45px', filter: 'invert(1)' }} alt="RoboSlog" className="img-fluid" /> */}
+                    <div className='mx-2 text-white' style={{ fontFamily: 'Manrope', letterSpacing: '2px' }}>HOME STAY</div></figure>
+                </a>
+            </div>
+            <div className="d-flex align-items-center justify-content-between me-2" style={{ minWidth: '250px', position: 'relative', zIndex: "1000", width: "5vw" }} id="navbarSupportedContent">
                 <div>
-                    <a class="navbar-brand" href="/">Home Stay</a>
+                    <a className="nav-item text-decoration-none text-white fw-bold" style={{ fontSize: '18px' }}>
+                        <NavLink className="nav-link" to="/">
+                            Home
+                        </NavLink>
+                    </a>
                 </div>
                 <div>
-                    <a class="navbar-brand btn btnStyling rounded-0" href="/">Home</a>
-                    <a class="navbar-brand btn btnStyling rounded-0" href="/">About</a>
-                    <a class="navbar-brand btn btnStyling rounded-0" href="/">Contact</a>
+                    <a className="nav-item text-decoration-none text-white fw-bold" style={{ fontSize: '18px' }}>
+                        <NavLink className="nav-link" to="/about">About</NavLink>
+                    </a>
+                </div>
+                <div>
+                    <a className="nav-item text-decoration-none text-white fw-bold" style={{ fontSize: '18px' }}>
+                        <NavLink className="nav-link" to="/contact">Contact Us</NavLink>
+                    </a>
                 </div>
 
             </div>
