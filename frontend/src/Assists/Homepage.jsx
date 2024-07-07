0221e175bcaeac044acc295d6df64de9ac015cba
anirudhkala110 <anirudhkala110@gmail.com>
2024-07-07 14:35:30 +0530

    Updated map show

--- frontend/src/Assists/Homepage.jsx
+++ frontend/src/Assists/Homepage.jsx
@@ -15,7 +15,7 @@ import TypewriterEffect from './TypeWriter'
 import WhyChooseUs from './WhyChooseUs'
 
 const Homepage = () => {
-    const [mapShow, setMapShow] = useState(false)
+    const [mapShow, setMapShow] = useState(true)
     return (
         <div className=' px-0 min-vh-100' style={{ minWidth: "400px" }}>
             <div className='less1000px align-items-center text-white px-2' style={{ background: "#000000a3", position: 'absolute', lineHeight: '4', top: '200px', zIndex: '100', width: '100%', minHeight: "320px", fontSize: "5rem" }}>
@@ -91,7 +91,7 @@ const Homepage = () => {
 
                 </div>
                 {
-                    mapShow && <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1296.6642496660997!2d78.69219255390766!3d29.944206701383614!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39099c4e50679007%3A0x36fd8fa768adc344!2sThe%20Hans%20Foundation%20General%20Hospital%20(THFGH)%2C%20Satpuli!5e0!3m2!1sen!2sin!4v1719912519677!5m2!1sen!2sin" width="100%" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    mapShow && <iframe className='my-1 shadow' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1296.6642496660997!2d78.69219255390766!3d29.944206701383614!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39099c4e50679007%3A0x36fd8fa768adc344!2sThe%20Hans%20Foundation%20General%20Hospital%20(THFGH)%2C%20Satpuli!5e0!3m2!1sen!2sin!4v1719912519677!5m2!1sen!2sin" width="100%" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 }
             </div>
         </div>
